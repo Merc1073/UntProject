b64aad225d834a8dadddc45e9d3c778e1b40f48e
Merc1073 <sergeikoblov2000@gmail.com>
2024-02-07 12:41:34 +0000

    Renaming scripts + Materials

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MainPlayer : MonoBehaviour
 {
 
     //public GameObject clone;
